04c86c3f4325551f4019f519db7c5209624d437f
lukebrandonfrancis <lukebrandonfrancis@gmail.com>
2018-07-24 23:59:23 -0400

    - Trying to Refactor Code... see program for details

--- PairProgram/Program.cs
+++ PairProgram/Program.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+    /*Write a C# Sharp program that takes the radius
+    of a sphere as input and calculate and display
+    the surface and volume of the sphere. Go to the editor */
+    //Test Data:
+    //Radius: 2 
+    //Expected Output:
+    //50.26548
+    //33.51032
+
 namespace PairProgram
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+
         }
     }
 }
---
+++ PairProgram/SurfAndVol.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace PairProgram
+{
+    
+
+    class SurfAndVol
+    {
+        private const double Pie = 3.14;
+        private const int Div = 4 / 3;
+        
+
+        public void CalcVolOfSphere(int radius)
+        {
+           // return Math.Pow(radius, 3);
+        }
+    }
+}
