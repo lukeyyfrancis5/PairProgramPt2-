31ab92a5f7c49c27435738a70b76c832aa2d5727
lukebrandonfrancis <lukebrandonfrancis@gmail.com>
2018-07-25 00:54:14 -0400

    - Tests fail because of the how  the methods handle decimals

--- PairProgram/SurfAndVol.cs
+++ PairProgram/SurfAndVol.cs
@@ -7,17 +7,28 @@ using Microsoft.Win32;
 
 namespace PairProgram
 {
-    
-
     class SurfAndVol
     {
-        private const double Pie = 3.14;
-        private const int Div = 4 / 3;
+        private const double pie = 3.14;
         
-
-        public void CalcVolOfSphere(int radius)
+        public double CalcVolume(int number)
         {
-           // return Math.Pow(radius, 3);
+            const double div = 4 / 3;
+
+            var radius = Math.Pow(number, 3);
+
+            var vol = div * (pie * radius);
+
+            return vol;
+        }
+
+        public double CalcSurface(int number)
+        {
+            var radius = Math.Pow(number, 2);
+
+            var surf = 4 * (pie * radius);
+
+            return surf;
         }
     }
 }
--- PairProgram/SurfAndVolTest.cs
+++ PairProgram/SurfAndVolTest.cs
@@ -10,10 +10,26 @@ namespace PairProgram
     [TestFixture]
     class SurfAndVolTest
     {
+        // THIS TEST FAILS (likely because of decimals)
         [Test]
-        public void Volume_CalculatesRadius_returnVolumeOfSphere()
+        public void Volume_CalculateRadius_returnVolumeOfSphere()
         {
+            SurfAndVol getVol = new SurfAndVol();
 
+            var result = getVol.CalcVolume(2);
+
+            Assert.That(result, Is.EqualTo(33.51));
+        }
+
+        // THIS TEST FAILS (likely because of decimals)
+        [Test]
+        public void SurFace_CalculateRadius_ReturnSurfaceOfSphere()
+        {
+            SurfAndVol getSurf = new SurfAndVol();
+
+            var result = getSurf.CalcSurface(1);
+
+            Assert.That(result, Is.EqualTo(12.7));
         }
     }
 }
